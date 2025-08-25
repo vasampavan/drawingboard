b59376136e4a385094d3877771bf1cbed0985e7f
Madhu890-SS <pavanvasam7@gmail.com>
2025-08-26 01:07:16 +0530

    added buttons and implemented action for buttons

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,15 +1,23 @@
 import React, { useRef, useEffect, useState } from "react";
+import Tools from "./Tools";
 
 import {
   StyledCanvas,
   StyledDiv,
+  ToolsDiv,
+  Div,
+  StyledInput,
   GlobalStyle,
-} from "./Styles.jsx";
+} from "./Styles";
 const Canvas = () => {
   const canvasRef = useRef(null);
   const [isDrawing, setIsDrawing] = useState(false);
   const [context, setContext] = useState(null);
   const [color, setColor] = useState("white");
+  const [lineWidth, setLineWidth] = useState(2);
+  const [drawingHistory, setDrawingHistory] = useState([]);
+  const [redoHistory, setRedoHistory] = useState([]);
+
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
@@ -32,18 +40,66 @@ const Canvas = () => {
     context.stroke();
   };
 
+  const stopDrawing = () => {
+    setIsDrawing(false);
+    const canvas = canvasRef.current;
+    const updatedDrawingHistory = [...drawingHistory];
+    updatedDrawingHistory.push(canvas.toDataURL());
+    setDrawingHistory(updatedDrawingHistory);
+    setRedoHistory([]);
+  };
+
   const clearCanvas = () => {
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
     setDrawingHistory([]);
     setRedoHistory([]);
   };
+
+  const redoDrawing = () => {
+    if (redoHistory.length > 0) {
+      const restoredDrawing = redoHistory.pop();
+      const updatedDrawingHistory = [...drawingHistory, restoredDrawing];
+      setDrawingHistory(updatedDrawingHistory);
+      const canvas = canvasRef.current;
+      const image = new Image();
+      image.onload = () => {
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+        context.drawImage(image, 0, 0);
+      };
+      image.src = restoredDrawing;
+      setRedoHistory(redoHistory);
+    }
+  };
+
   const erase = () => {
     setColor("black"); 
   };
 
+  const undoDrawing = () => {
+    if (drawingHistory.length > 0) {
+      const undoneDrawing = drawingHistory.pop();
+      const updatedRedoHistory = [...redoHistory, undoneDrawing];
+      setRedoHistory(updatedRedoHistory);
+      const canvas = canvasRef.current;
+      const image = new Image();
+      image.onload = () => {
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+        context.drawImage(image, 0, 0);
+      };
+      image.src = drawingHistory[drawingHistory.length - 1];
+      setDrawingHistory(drawingHistory);
+    }
+  };
 
-
- 
+  const saveImage = () => {
+    const canvas = canvasRef.current;
+    const link = document.createElement("a");
+    link.href = canvas.toDataURL();
+    const fileName = prompt("enter a name to save file");
+    link.download = `${fileName}`;
+    link.download ? alert("saved successfully") : alert("problem");
+    link.click();
+  };
 
   return (
     <>
@@ -55,10 +111,38 @@ const Canvas = () => {
           height={700}
           onMouseDown={startDrawing}
           onMouseMove={draw}
+          onMouseUp={stopDrawing}
+          onMouseLeave={stopDrawing}
           onTouchStart={startDrawing}
           onTouchMove={draw}
+          onTouchEnd={stopDrawing}
           title='Start drawing'
         />
+        <ToolsDiv>
+          <StyledInput
+            type='color'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+          />
+          <StyledInput
+            type='range'
+            min={1}
+            max={20}
+            value={lineWidth}
+            onChange={(e) => setLineWidth(parseInt(e.target.value))}
+          />
+          <Div>
+            <Tools
+              method_props={{
+                saveImage_prop: saveImage,
+                clearCanvas_prop: clearCanvas,
+                erase_prop: erase,
+                undo_props: undoDrawing,
+                redo_props: redoDrawing,
+              }}
+            />
+          </Div>
+        </ToolsDiv>
       </StyledDiv>
     </>
   );
--- src/components/Styles.jsx
+++ src/components/Styles.jsx
@@ -9,6 +9,30 @@ export const StyledCanvas = styled.canvas`
     cursor: pointer;
   }
 `;
+
+export const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+export const ToolsDiv = styled(StyledDiv)`
+  background-color: black;
+  padding: 10px 100px;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 10px;
+`;
+export const Div = styled(ToolsDiv)`
+  display: flex;
+  justify-content: space-between;
+  /* background-color: transparent; */
+`;
+export const StyledInput = styled.input`
+  padding: ${(prop) =>
+    prop.type === "color" ? "0px 10px" : "2px 10px 2px 10px"};
+  margin: 0px 20px;
+`;
 export const GlobalStyle = createGlobalStyle`
 body{
     background: black;
@@ -16,9 +40,19 @@ body{
     margin: 0px;
 }
 `;
-export const StyledDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+
+export const Button = styled.button`
+  margin: 0px 5px 0px 5px;
+  padding: 10px;
+  border-radius: 10px;
+  background-color: rgb(39 123 115);
+  color: black;
+
+  &:hover {
+    transition: color 2s, background-color 2s, text-transform 3s;
+    color: limegreen;
+    background-color: rgb(39 123 15);
+    text-transform: uppercase;
+    box-shadow: 1px 1px 5px rgb(30 100 150) ;
+  }
 `;
