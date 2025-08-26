baba4aa275cf41b0e492e38552ebd024f4367939
Madhu890-SS <pavanvasam7@gmail.com>
2025-08-27 00:46:06 +0530

    added colorpicker feature

---
+++ src/components/ColorPicker.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+const ColorPicker = ({ onSelectColor }) => {
+  const [selectedColor, setSelectedColor] = useState("black");
+  const [recentColors, setRecentColors] = useState([]);
+
+  const handleColorSelect = (color) => {
+    setSelectedColor(color);
+    onSelectColor(ycolor);
+    if (!recentColors.includes(color)) {
+      const updatedRecentColors = [color, ...recentColors.slice(0, 4)];
+      setRecentColors(updatedRecentColors);
+    }
+  };
+
+  const handleCustomColorChange = (event) => {
+    const color = event.target.value;
+    setSelectedColor(color);
+    onSelectColor(color);
+  };
+
+  const handleRecentColorSelect = (color) => {
+    setSelectedColor(color);
+    onSelectColor(color);
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => handleColorSelect("black")}>Black</button>
+        <button onClick={() => handleColorSelect("red")}>Red</button>
+        <button onClick={() => handleColorSelect("blue")}>Blue</button>
+      </div>
+      <div>
+        <label htmlFor='customColor'>Custom Color:</label>
+        <input
+          type='color'
+          id='customColor'
+          value={selectedColor}
+          onChange={handleCustomColorChange}
+        />
+      </div>
+      <div>
+        <p>Recent Colors:</p>
+        {recentColors.map((color, index) => (
+          <button key={index} onClick={() => handleRecentColorSelect(color)}>
+            {color}
+          </button>
+        ))}
+      </div>
+
+    </div>
+  );
+};
+
+export default ColorPicker;
